2bd368cba00b55d72b0d03d7d75469b309540ddb
Muttaky <muttaky9@gmail.com>
2022-02-17 22:52:24 +0600

    almost

--- money.js
+++ money.js
@@ -1,4 +1,4 @@
-document.getElementById('calc').addEventListener('click',function()
+document.getElementById('calcu').addEventListener('click',function()
 {
     let income=document.getElementById('income');
     let incomeNum= parseInt(income.value);
@@ -17,4 +17,20 @@ document.getElementById('calc').addEventListener('click',function()
     totalExpenses.innerText=foodNum+clothsNum+rentNum;
     balance.innerText=incomeNum-totalExpenses.innerText
 
+})
+
+document.getElementById('save').addEventListener('click',function(){
+
+    let saving=document.getElementById('saving');
+    let savingNum=parseInt(saving.value);
+
+    let savingAmount=document.getElementById('savingAmount');
+    let remainingBalance=document.getElementById('remainingBalance');
+
+    let income=document.getElementById('income');
+    let incomeNum=parseInt(income.value);
+    let per=incomeNum / 100;
+    
+    savingAmount.innerText=savingNum*per;
+    remainingBalance.innerText=balance.innerText-savingAmount.innerText;
 })
